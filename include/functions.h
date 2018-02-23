c7c49800bb79249987d8f0461b7a02e420a48922
honor4him <zach711595@hotmail.com>
2018-02-23 10:40:39 -0500

    Add comments

--- include/functions.h
+++ include/functions.h
@@ -1,6 +1,7 @@
 #include <string.h>
 #include <iostream>
 
+// .h file defining functions for functions.cpp by Zachary Thomas
 
 bool balancedSymbols(std::string input);
 std::string iToP(std::string input);
