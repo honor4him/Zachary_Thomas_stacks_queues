23d7231d70339759a92508c0f034e2cd25513555
honor4him <zach711595@hotmail.com>
2018-02-22 21:28:34 -0500

    Added commented code for functions.cpp

--- src/functions.cpp
+++ src/functions.cpp
@@ -1,39 +1,49 @@
 #include <iostream>
 #include <stack>
 #include "functions.h"
+/********************************************************************
+ * This is an assignment for CIS 263, Data Structures and Algorithms.
+ * The first primary function determines ending braces, parentheses, 
+ * and brackets. The second primary function takes an infix string
+ * and converts it to postfix. Both of these functions use stacks
+ * as their primary data structure.
+ *
+ * @author Zachary Thomas
+ * @version 1.0
+ *******************************************************************/
 
+/******************************************************************
+ * Helper function for iToP to determine the order of operations.
+ * @param top char variable
+ * @param compare char variable
+ * @return boolean If top is higher or lower.
+ *****************************************************************/
 bool highPriority(char top, char compare);
 
+/******************************************************************
+ * Helper function for highPriority that assigns a weight of an
+ * integer for an operator.
+ * @param opo An operator in char format to assign value to.
+ * @return int The value of the weight
+ *****************************************************************/
 int getWeight(char opo);
 
-int main(int argc, char* argv[]){
-
-	std::string arb = "{abc} [1] something here ({[123]})";
-	std::string wrong = "([)]";
-	
-	std::string infix = "a + b * c + ( d * e + f) * g";
-	std::string prefix;
-	
-	// Should be true
-	std::cout << "The first string is:  " << balancedSymbols(arb) << std::endl;
-	
-	// Should be false
-	std::cout << "The second string is: " << balancedSymbols(wrong) << std::endl;	
-
-	std::cout << iToP(infix) << std::endl; 
-}
-
-
+/******************************************************************
+ * This function, balancedSymbols, takes in a string that may or
+ * may not have parentheses. It checks to see if there is always
+ * a corresponding ending to a respective symbol.
+ * @param input A string that has the symbols in it.
+ * @return bool Returns true if there is a closing symbol(s).
+ *****************************************************************/
 bool balancedSymbols(std::string input){
 	
+	/** Holds the stack of symbols */
 	std::stack<char, std::deque<char>> symbols;
 	
 	// Loop through each character in the string
 	for(int i = 0; i < input.length(); i++){
 		if(input[i] == '[' || input[i] == '{' || input[i] == '('){
 			symbols.push(input[i]);		
-			
-			std::cout << symbols.top() << std::endl;
 		}
 
 		// Test ending expression
@@ -57,9 +67,20 @@ bool balancedSymbols(std::string input){
 	return symbols.empty();
 }
 
+/******************************************************************
+ * This function, iToP, takes an infix expression and converts it
+ * using a stack. This takes the order of operations and puts the
+ * string into postfix. This function also works with parentheses.
+ * Help from https://www.youtube.com/watch?v=vq-nUF0G4fI
+ * @param input A string that is to convert to postfix.
+ * @return string Returns postfix expression.
+ *****************************************************************/
 std::string iToP(std::string input){
 
-	std::stack<char, std::deque<char>> postStack;	
+	/** Holds the operators to add to postfix */
+	std::stack<char, std::deque<char>> postStack;
+	
+	/** Holds the postfix expression */
 	std::string postfix;
 	
 	for(int i = 0; i < input.length(); i++){
@@ -110,16 +131,26 @@ std::string iToP(std::string input){
 		postStack.pop();
 	}
 
+	// Returns finished postfix string
 	return postfix;
 }
 
-
+/******************************************************************
+ * Helper function for iToP to determine the order of operations.
+ * @param top char variable
+ * @param compare char variable
+ * @return boolean If top is higher or lower.
+ *****************************************************************/
 bool highPriority(char top, char compare){
 	
+	/** Holds the weight of top*/
 	int topWeight = getWeight(top);
+	
+	/** Holds the weight of compare*/
 	int compareWeight = getWeight(compare);
 	
 	// Compare weight to see if the operators need to be added to postfix string
+	// According to the order of operations
 	if(topWeight == compareWeight)
 		return true;
 	else if (topWeight > compareWeight)
@@ -128,8 +159,15 @@ bool highPriority(char top, char compare){
 		return false; 	
 }
 
+/******************************************************************
+ * Helper function for highPriority that assigns a weight of an
+ * integer for an operator.
+ * @param opo An operator in char format to assign value to.
+ * @return int The value of the weight
+ *****************************************************************/
 int getWeight(char opo){
 	
+	/** Holds the weight of opo*/
 	int weight = 0;
 	
 	// Go through each operator to see which one takes order of operations
@@ -144,6 +182,7 @@ int getWeight(char opo){
 			break;
 	}
 	
+	// Returns the end weight
 	return weight;
 }
 
