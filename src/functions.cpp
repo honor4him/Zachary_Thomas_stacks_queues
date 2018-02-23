9c2f37dd6a0689436799504d02203a8da2cb879d
honor4him <zach711595@hotmail.com>
2018-02-22 20:52:43 -0500

    Add files via upload
    Done, just have to add java style guide commenting

--- src/functions.cpp
+++ src/functions.cpp
@@ -2,12 +2,16 @@
 #include <stack>
 #include "functions.h"
 
+bool highPriority(char top, char compare);
+
+int getWeight(char opo);
+
 int main(int argc, char* argv[]){
 
 	std::string arb = "{abc} [1] something here ({[123]})";
 	std::string wrong = "([)]";
 	
-	std::string infix = " 3 + 2 * 5 - 2 / 2"; // = 12
+	std::string infix = "a + b * c + ( d * e + f) * g";
 	std::string prefix;
 	
 	// Should be true
@@ -16,6 +20,7 @@ int main(int argc, char* argv[]){
 	// Should be false
 	std::cout << "The second string is: " << balancedSymbols(wrong) << std::endl;	
 
+	std::cout << iToP(infix) << std::endl; 
 }
 
 
@@ -53,8 +58,101 @@ bool balancedSymbols(std::string input){
 }
 
 std::string iToP(std::string input){
-	
+
+	std::stack<char, std::deque<char>> postStack;	
 	std::string postfix;
+	
+	for(int i = 0; i < input.length(); i++){
+		
+		// Skip current iteration if input has a space
+		if(input[i] == ' ')
+			continue;
+
+		// See if char is an operator
+		if(input[i] == '*' || input[i] == '/' || input[i] == '+' || input[i] == '-'){
+			
+			// Only push if no parentheses
+			while(!postStack.empty() && postStack.top() != '(' && highPriority(postStack.top(), input[i])){
+				postfix += postStack.top();
+				postStack.pop();
+			}
+
+			// Insert operator
+			postStack.push(input[i]);			
+		}
+		
+		// Push if starting of parentheses
+		else if(input[i] == '(')
+			postStack.push(input[i]);
+		
+		// Push if ending of parentheses
+		else if(input[i] == ')'){
+			
+			// Add operators to postfix
+			while(!postStack.empty() && postStack.top() != '('){
+				postfix += postStack.top();
+				postStack.pop();
+			}
+
+			// Pop remaining parentheses
+			postStack.pop();
+		}
+		
+		// Add any remaining characters or integers to postfix if not an operator	  				
+		else
+			postfix += input[i];
+			
+	}
+
+	// Add remaining operators
+	while(!postStack.empty()){
+		postfix += postStack.top();
+		postStack.pop();
+	}
 
 	return postfix;
 }
+
+
+bool highPriority(char top, char compare){
+	
+	int topWeight = getWeight(top);
+	int compareWeight = getWeight(compare);
+	
+	// Compare weight to see if the operators need to be added to postfix string
+	if(topWeight == compareWeight)
+		return true;
+	else if (topWeight > compareWeight)
+		return true;
+	else
+		return false; 	
+}
+
+int getWeight(char opo){
+	
+	int weight = 0;
+	
+	// Go through each operator to see which one takes order of operations
+	switch(opo){
+		case '+':
+		case '-':
+			weight = 1;
+			break;
+		case '*':
+		case '/':
+			weight = 2;
+			break;
+	}
+	
+	return weight;
+}
+
+
+
+
+
+
+
+
+
+
